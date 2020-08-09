cf956269097659b9a7f0400cdd91cdc9cd5e00ab
Nate Faber <nathaniel@njfaber.com>
2020-08-09 15:44:24 +0200

    Make issue function and only let owner use it

--- contracts/C2.sol
+++ contracts/C2.sol
@@ -1,6 +1,12 @@
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/access/Ownable.sol';
 
-contract C2 is ERC20 {
+contract C2 is ERC20, Ownable {
     constructor() ERC20("ContributorCredits", "C^2") public{
+    }
+
+    
+    function issue(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
     }
 }
--- test/C2.js
+++ test/C2.js
@@ -1,4 +1,6 @@
+
 const C2 = artifacts.require("C2");
+const truffleAssert = require('truffle-assertions');
 
 contract("C2", async (accounts) => {
     it("shouldn't have any tokens to start", async () => {
@@ -9,9 +11,20 @@ contract("C2", async (accounts) => {
             0,
             "tokens were created when none should have been"
         );
+        });
+    it("can issue tokens", async () => {
+        const instance = await C2.deployed();
+        await instance.issue(accounts[1], 1);
+        
+        const account1Bal = await instance.balanceOf.call(accounts[1]);
+        assert.equal(account1Bal.toNumber(), 1, "1 token was not issued");
 
-    it("should only allow transfers to and from owner"), () => {
-        assert.fail()
-    }
     });
+    it("should only allow the owner to issue tokens", async() => {
+        const instance = await C2.deployed({ from: accounts[0] });
+        truffleAssert.reverts(instance.issue(accounts[1], 1, { from: accounts[1] }))
+    })
+    // it("should only allow transfers to and from owner", () => {
+    //     assert.fail();
+    // });
 });
