2d383354c8dbf1169ab5a0d917ca01675b873c1c
Nabin <nabin@sunshine-labs.com>
2020-08-15 13:47:17 +0000

    Implment burn

--- contracts/C2.sol
+++ contracts/C2.sol
@@ -1,12 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.8;
+
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import 'openzeppelin-solidity/contracts/access/Ownable.sol';
 
 contract C2 is ERC20, Ownable {
-    constructor() ERC20("ContributorCredits", "C^2") public{
-    }
 
+    constructor() ERC20("ContributorCredits", "C^2") public {
+    }
     
     function issue(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
     }
 }
--- test/C2.js
+++ test/C2.js
@@ -2,28 +2,41 @@
 const C2 = artifacts.require("C2");
 const truffleAssert = require('truffle-assertions');
 
+const assertBalance = async (instance, addr, amount) => {
+    const bal = await instance.balanceOf.call(addr);
+    assert.equal(bal.toNumber(), amount, `Balance is ${bal.toNumber()}, not ${amount}`);
+}
+
 contract("C2", async (accounts) => {
     it("shouldn't have any tokens to start", async () => {
         const instance = await C2.deployed();
         const totalSupply = await instance.totalSupply.call();
         assert.equal(
-            totalSupply.toNumber(),
-            0,
-            "tokens were created when none should have been"
-        );
-        });
+                totalSupply.toNumber(),
+                0,
+                "tokens were created when none should have been"
+                );
+    });
     it("can issue tokens", async () => {
         const instance = await C2.deployed();
         await instance.issue(accounts[1], 1);
-        
-        const account1Bal = await instance.balanceOf.call(accounts[1]);
-        assert.equal(account1Bal.toNumber(), 1, "1 token was not issued");
 
+        await assertBalance(instance, accounts[1], 1);
     });
     it("should only allow the owner to issue tokens", async() => {
         const instance = await C2.deployed({ from: accounts[0] });
         truffleAssert.reverts(instance.issue(accounts[1], 1, { from: accounts[1] }))
     })
+    it("can relinquish tokens", async() => {
+        const instance = await C2.deployed({ from: accounts[0] });
+        await instance.issue(accounts[1], 9);
+
+        await assertBalance(instance, accounts[1], 10);
+        
+        await instance.burn(1, { from: accounts[1] });
+
+        await assertBalance(instance, accounts[1], 9);
+    });
     // it("should only allow transfers to and from owner", () => {
     //     assert.fail();
     // });
