40da917d790ef8150ba071688e0f8bf16458329b
Nabin <nabin@sunshine-labs.com>
2020-08-15 14:19:03 +0000

    Introduce backing token

--- contracts/C2.sol
+++ contracts/C2.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.6.8;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
 import 'openzeppelin-solidity/contracts/access/Ownable.sol';
 
 contract C2 is ERC20, Ownable {
 
-    constructor() ERC20("ContributorCredits", "C^2") public {
+    IERC20 private _backingToken;
+
+    constructor(IERC20 backingToken) ERC20("ContributorCredits", "C^2") public {
+        _backingToken = backingToken;
     }
     
     function issue(address account, uint256 amount) public onlyOwner {
--- migrations/2_init_c2.js
+++ migrations/2_init_c2.js
@@ -1,5 +1,5 @@
 var C2 = artifacts.require("C2");
 
 module.exports = function(deployer) {
-    deployer.deploy(C2);
+    deployer.deploy(C2, "0x0000000000000000000000000000000000000000");
 }
