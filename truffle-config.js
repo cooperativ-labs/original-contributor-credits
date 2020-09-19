af4d4d3c7e4440ed143c38b624e3a0e1aa098e35
Robin Raymond <robin@robinraymond.de>
2020-09-19 13:23:44 +0200

    Linting

--- truffle-config.js
+++ truffle-config.js
@@ -62,19 +62,19 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-    provider: () => new HDWalletProvider(ROPSTEN_ACCOUNT_KEY, `https://ropsten.infura.io/v3/${INFURA_KEY}`),
-    network_id: 3,       // Ropsten's id
-    gas: 4000000,        // Ropsten has a lower block limit than mainnet
-    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-    gasPrice: web3.utils.toWei('225', 'gwei')
-  },
+      provider: () => new HDWalletProvider(ROPSTEN_ACCOUNT_KEY, `https://ropsten.infura.io/v3/${INFURA_KEY}`),
+      network_id: 3,       // Ropsten's id
+      gas: 4000000,        // Ropsten has a lower block limit than mainnet
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      gasPrice: web3.utils.toWei('225', 'gwei')
+    },
     live: {
       provider: () => new HDWalletProvider(MAINNET_ACCOUNT_KEY, `https://mainnet.infura.io/v3/${INFURA_KEY}`),
-      network_id: 1,       // Ropsten's id
-      gas: 4000000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 2000,  // # of blocks before a deployment times out  (minimum/default: 50)
+      network_id: 1,
+      gas: 4000000,
+      confirmations: 2,
+      timeoutBlocks: 2000,
       gasPrice: web3.utils.toWei('225', 'gwei')
     },
     // Useful for private networks
