f3760ca5c60e5b0bd65076e9df16439a93ad1ee8
Nate Faber <nathaniel@njfaber.com>
2020-09-19 12:31:30 +0200

    Add event emitting

--- contracts/C2.sol
+++ contracts/C2.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.8;
 
-
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
-import 'openzeppelin-solidity/contracts/access/Ownable.sol';
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract C2 is ERC20, Ownable {
-
     IERC20 private _backingToken;
     using SafeMath for uint256;
 
     bool _isLive = false;
     modifier isLive() {
-        require(_isLive == true, "token must be established with a ratio first");
+        require(
+            _isLive == true,
+            "token must be established with a ratio first"
+        );
         _;
     }
     modifier isNotLive() {
@@ -22,32 +23,57 @@ contract C2 is ERC20, Ownable {
         _;
     }
 
-    constructor(IERC20 backingToken) ERC20("ContributorCredits", "C^2") public {
+    constructor(IERC20 backingToken) public ERC20("ContributorCredits", "C^2") {
         _backingToken = backingToken;
     }
 
-    function establish(uint256 initialBac, uint256 initialC2) public onlyOwner isNotLive {
+    function establish(uint256 initialBac, uint256 initialC2)
+        public
+        onlyOwner
+        isNotLive
+    {
         _backingToken.transferFrom(this.owner(), address(this), initialBac);
         _mint(this.owner(), initialC2);
         _isLive = true;
     }
 
+    event Issued(
+        address indexed account,
+        uint256 c2Issued,
+        uint256 backingAmount
+    );
+
     function issue(address account, uint256 amount) public onlyOwner isLive {
         uint256 backingNeeded = _backingNeededFor(amount);
         _backingToken.transferFrom(_msgSender(), address(this), backingNeeded);
         _mint(account, amount);
+        emit Issued(account, amount, backingNeeded);
     }
+
+    event Burned(
+        address indexed account,
+        uint256 c2Burned,
+        uint256 backingReturned
+    );
 
     function burn(uint256 amount) public isLive {
         uint256 associatedBacking = _backingNeededFor(amount);
         _backingToken.transfer(this.owner(), associatedBacking);
         _burn(_msgSender(), amount);
+        emit Burned(_msgSender(), amount, associatedBacking);
     }
+
+    event CashedOut(
+        address indexed account,
+        uint256 c2Exchanged,
+        unit256 backingReceived
+    );
 
     function cashout(uint256 amount) public isLive {
         uint256 associatedBacking = _backingNeededFor(amount);
         _backingToken.transfer(_msgSender(), associatedBacking);
         _burn(_msgSender(), amount);
+        emit CashedOut(_msgSender(), amount, backingReturned)
     }
 
     function _bacBalance() internal view returns (uint256) {
@@ -56,7 +82,6 @@ contract C2 is ERC20, Ownable {
 
     function _backingNeededFor(uint256 amountC2) public view returns (uint256) {
         // The -1 +1 is to get the ceiling division, rather than the floor so that you always err on the side of having more backing
-        return amountC2.mul(_bacBalance()).sub(1).div(totalSupply()).add(1); 
+        return amountC2.mul(_bacBalance()).sub(1).div(totalSupply()).add(1);
     }
-
 }
