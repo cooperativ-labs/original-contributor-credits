122f2112c1bf4b2b38157be275b7e199b775633f
Nate Faber <nathaniel@njfaber.com>
2020-11-08 17:13:23 +0100

    Remove underscores and clean up code (#20)
    * Remove underscores and clean up names
    
    * Lint code
    
    * Add linting step to gh actions
    
    * Lint code

--- migrations/2_init_c2.js
+++ migrations/2_init_c2.js
@@ -4,15 +4,14 @@ var BackingToken21 = artifacts.require("BackingToken21");
 var BackingToken15 = artifacts.require("BackingToken15");
 var BackingToken6 = artifacts.require("BackingToken6");
 
-module.exports = function(deployer, network, accounts) {
-
-    if (network === "live") {
-        return deployer.deploy(C2);
-    } else {
-        deployer.deploy(BackingToken, accounts[0])
-        deployer.deploy(BackingToken21, accounts[0])
-        deployer.deploy(BackingToken15, accounts[0])
-        deployer.deploy(BackingToken6, accounts[0])
-        return deployer.deploy(C2);
-    }
-}
+module.exports = function (deployer, network, accounts) {
+  if (network === "live") {
+    return deployer.deploy(C2);
+  } else {
+    deployer.deploy(BackingToken, accounts[0]);
+    deployer.deploy(BackingToken21, accounts[0]);
+    deployer.deploy(BackingToken15, accounts[0]);
+    deployer.deploy(BackingToken6, accounts[0]);
+    return deployer.deploy(C2);
+  }
+};
